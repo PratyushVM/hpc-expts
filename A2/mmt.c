8d4ff914fbbbe1589e977dd83123978fd96e5bbf
PratyushMoorthy <pratyush.v.m@gmail.com>
2020-03-25 06:40:36 +0530

    Improve mmt.c

--- A2/mmt.c
+++ A2/mmt.c
@@ -1,9 +1,10 @@
+#define size 4096
 void parallel_mmt(int n,float c[][n],float a[][n], float b[][n],int nThreads){
   int i,j,k;
   omp_set_num_threads(nThreads);
-  #pragma omp parallel private(i,j,k)
+  #pragma omp parallel private(i,j,k) shared(a,b,c)
   {
-      #pragma omp for
+      #pragma omp for collapse(2)
       for(j=0;j<n;j++)
         for(k=0;k<n;k++)
             for(i=0;i<n;i++)
